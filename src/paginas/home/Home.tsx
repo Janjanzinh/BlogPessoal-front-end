eadb28e328d69f70951de04dfd74a390d46f3c84
Janilson Alves Romualdo <janilson1571@gmail.com>
2023-05-12 11:23:48 -0300

    Primeiros passos de front"

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import App from "../../App";
+
+function Home(){
+    return(
+        <>
+            <h1>Home</h1>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fbr%2Ffoto%2Fador%25C3%25A1vel-cachorro-de-%25C3%25B3culos-trabalhando-com-computador-gm1214325113-353256203&psig=AOvVaw2TE1GiSqZ3jsYmy576klNI&ust=1683941932626000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMDZ0NfS7v4CFQAAAAAdAAAAABAE" alt="Imagem Tela Inicial" />
+            
+        </>
+    );
+}
+export default Home;
