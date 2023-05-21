d14189d3b3c72d9646889fb43c6ce8168d95b841
Janilson Alves Romualdo <janilson1571@gmail.com>
2023-05-21 16:30:23 -0300

    Colocando as rotas do login

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React from "react";
-import Navbar from "./components/estaticos/navbar/Navbar";
-import Footer from "./components/estaticos/footer/Footer";
-import {Grid} from '@material-ui/core'
-import Home from "./paginas/home/Home";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+import './App.css';
+import Login from './paginas/login/Login';
 
 
-function App(){
-   return(
-    <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
-    </>
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </Router>
+
   );
 }
 
